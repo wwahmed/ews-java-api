2845287ec5fec4e828cc26a68959f7623e622150
wahmed <wwahmed@gmail.com>
2016-04-10 01:38:44 -0400

    Support read and delivery receipt on meeting cancellation messages.

--- src/main/java/microsoft/exchange/webservices/data/core/service/response/CancelMeetingMessage.java
+++ src/main/java/microsoft/exchange/webservices/data/core/service/response/CancelMeetingMessage.java
@@ -28,6 +28,7 @@ import microsoft.exchange.webservices.data.core.XmlElementNames;
 import microsoft.exchange.webservices.data.core.service.item.Item;
 import microsoft.exchange.webservices.data.core.service.item.MeetingCancellation;
 import microsoft.exchange.webservices.data.core.service.schema.CancelMeetingMessageSchema;
+import microsoft.exchange.webservices.data.core.service.schema.EmailMessageSchema;
 import microsoft.exchange.webservices.data.core.service.schema.ServiceObjectSchema;
 import microsoft.exchange.webservices.data.core.enumeration.misc.ExchangeVersion;
 import microsoft.exchange.webservices.data.core.exception.service.local.ServiceLocalException;
@@ -91,5 +92,18 @@ public final class CancelMeetingMessage extends
     this.getPropertyBag().setObjectFromPropertyDefinition(
         CancelMeetingMessageSchema.Body, value);
   }
+
+  public void setIsReadReceiptRequested(boolean value) throws Exception
+  {
+    this.getPropertyBag().
+        setObjectFromPropertyDefinition(EmailMessageSchema.IsReadReceiptRequested, value);
+  }
+
+  public void setIsDeliveryReceiptRequested(boolean value) throws Exception
+  {
+    this.getPropertyBag().
+        setObjectFromPropertyDefinition(EmailMessageSchema.IsDeliveryReceiptRequested, value);
+  }
+
 
 }
